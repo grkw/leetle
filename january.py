fb6f1f52afadded180ba424cab402c0f0f668004
Grace Kwak <gracekwak0@gmail.com>
2025-01-07 13:12:34 -0500

    Valid parenthesis

--- january.py
+++ january.py
@@ -18,4 +18,23 @@ def max_subarray_kadane(nums):
         current_sum = max(num, current_sum + num) # With each new element, you can either start fresh (num) or append the new element to the contiguous subarray (current_sum + num).
         max_sum = max(max_sum, current_sum) # The maximum subarray thus far might not end at that element (it might end sooner) 
     return max_sum
-              
+             
+# 7. Valid Parenthesis
+# With Mia
+def valid_parens(s):
+  stack = []
+  bracket_dict = {
+    '(':')',
+    '{':'}',
+    '[':']'
+  }
+
+  for char in s:
+    if char in bracket_dict.keys(): # Push openers onto the stack
+      stack.append(char)
+    elif char in bracket_dict.values():
+      if not stack or char != bracket_dict.get(stack.pop()): # Check that the closers match the openers in stack order
+        return False
+    else:
+      pass
+  return not stack # Stack should be empty at the end because all openers should have closers
